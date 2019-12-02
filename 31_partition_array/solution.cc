f4dbbd9d745c4ba4c2b9af8e94b48ee9120b6fd9
Yongtian Zhang <ytzhang@ytzhang-macbookpro2.roam.corp.google.com>
2019-12-01 23:11:27 -0800

    .

---
+++ 31_partition_array/solution.cc
@@ -0,0 +1,23 @@
+class Solution {
+public:
+  /**
+   * @param nums: The integer array you should partition
+   * @param k: An integer
+   * @return: The index after partition
+   */
+  int partitionArray(vector<int> &nums, int k) {
+    // write your code here
+    if (nums.empty()) {
+      return 0;
+    }
+    int low = 0, high = nums.size() - 1;
+    for (; low <= high ;) {
+      for (; low <= high && nums[low] < k; ++low) {}
+      for (; low <= high && nums[high] >= k; --high) {}
+      if (low <= high) {
+        std::swap(nums[low], nums[high]);
+      }
+    }
+    return low;
+  }
+};
